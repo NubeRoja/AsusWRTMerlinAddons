8d5a6ec5749b3956a51d71c581406042d3e39d94
NubeRoja <svalenciano@gmail.com>
2017-04-07 01:28:00 +0200

    Update entware-ngu-setup.sh
    Update code
    Simplify backup code with bakcup function
    Simplify swap file creation code with swapfile function
    Added automatic pre-mount script to check ext/ntfs filesystem or mount swap partition
    Added services check option

--- contrib/entware-ngu-setup.sh
+++ contrib/entware-ngu-setup.sh
@@ -9,145 +9,142 @@ ERROR="$BOLD *** Error: $NORM"
 WARNING="$BOLD * Warning: $NORM"
 INPUT="$BOLD => $NORM"
 
-i=1 # Will count available partitions (+ 1)
-cd /tmp
+backup() {
+        echo -e "$WARNING Found previous $2 installation, saving..."
+        backupdir="$1"-bak_"$(date +'%F_%H-%M')"
+        mv "$1" "$backupdir"
+        echo -e "$INFO Backup of $1 created in $backupdir"
+        sleep 2
+}
 
-echo -e $INFO This script was created by ryzhov_al and modified by TeHashX.
-echo -e $INFO Thanks @zyxmon \& @ryzhov_al for New Generation Entware
-echo -e $INFO and @Rmerlin for his awesome firmwares
+swapfile() {
+        echo -e "$INFO Creating a $1 MB swap file..."
+        echo -e "$INFO This could take a while, be patient..."
+        dd if=/dev/zero of=/opt/swap bs=1024 count=$((1024*$1))
+        mkswap /opt/swap
+        chmod 0600 /opt/swap
+        swapon /opt/swap
+        echo -n "Press [Enter] key to continue..."
+        read readEnterKey
+}
+
+cd /tmp || exit 0
+
+clear
+echo -e "$INFO This script was created by ryzhov_al and modified by TeHashX."
+echo -e "$INFO Thanks @zyxmon \& @ryzhov_al for New Generation Entware"
+echo -e "$INFO and @Rmerlin for his awesome firmwares"
 sleep 2
-echo -e $INFO This script will guide you through the Entware-NG installation.
-echo -e $INFO Script modifies only \"entware-ng\" folder on the chosen drive,
-echo -e $INFO no other data will be touched. Existing installation will be
-echo -e $INFO replaced with this one. Also some start scripts will be installed,
-echo -e $INFO the old ones will be saved on partition where Entware-NG is installed
-echo -e $INFO like /tmp/mnt/sda1/jffs_scripts_backup.tgz
+echo -e "$INFO This script will guide you through the Entware-NG installation."
+echo -e "$INFO Script modifies only \"entware-ng\" folder on the chosen drive,"
+echo -e "$INFO no other data will be touched. Existing installation will be"
+echo -e "$INFO replaced with this one. Also some start scripts will be installed,"
+echo -e "$INFO the old ones will be saved on partition where Entware-NG is installed"
+echo -e "$INFO like /tmp/mnt/sda1/jffs_scripts_backup.tgz"
 echo
 
 if [ ! -d /jffs/scripts ] ; then
-  echo -e "$ERROR Please \"Enable JFFS partition\" from \"Administration > System\""
-  echo -e "$ERROR from router web UI: www.asusrouter.com/Advanced_System_Content.asp"
-  echo -e "$ERROR then reboot router and try again. Exiting..."
-  exit 1
+        echo -e "$ERROR Please \"Enable JFFS partition\" from \"Administration > System\""
+        echo -e "$ERROR from router web UI: www.asusrouter.com/Advanced_System_Content.asp"
+        echo -e "$ERROR then reboot router and try again. Exiting..."
+        exit 1
 fi
 
 case $(uname -m) in
-  armv7l)
-    PART_TYPES='ext2|ext3|ext4'
-    INST_URL='http://pkg.entware.net/binaries/armv7/installer/entware_install.sh'
-    ENT_FOLD='entware-ng.arm'
-    ;;
-  mips)
-    PART_TYPES='ext2|ext3'
-    INST_URL='http://pkg.entware.net/binaries/mipsel/installer/installer.sh'
-    ENT_FOLD='entware-ng'
-    ;;
-  *)
-    echo "This is unsupported platform, sorry."
-    ;;
+        armv7l)
+                PART_TYPES='ext2|ext3|ext4'
+                INST_URL='http://pkg.entware.net/binaries/armv7/installer/entware_install.sh'
+                ENT_FOLD='entware.arm'
+                ENTNG_FOLD='entware-ng.arm'
+                OPT_FOLD='asusware.arm'
+                OPTNG_FOLD='optware-ng.arm'
+                ;;
+        mips)
+                PART_TYPES='ext2|ext3'
+                INST_URL='http://pkg.entware.net/binaries/mipsel/installer/installer.sh'
+                ENT_FOLD='entware'
+                ENTNG_FOLD='entware-ng'
+                OPT_FOLD='asusware'
+                OPTNG_FOLD='optware-ng'
+                ;;
+        *)
+                echo "This is unsupported platform, sorry."
+                ;;
 esac
 
+i=1 # Will count available partitions (+ 1)
 echo -e "$INFO Looking for available partitions..."
-for mounted in `/bin/mount | grep -E "$PART_TYPES" | cut -d" " -f3` ; do
-  isPartitionFound="true"
-  echo "[$i] --> $mounted"
-  eval mounts$i=$mounted
-  i=`expr $i + 1`
+for mounted in $(/bin/mount | grep -E "$PART_TYPES" | cut -d" " -f3) ; do
+        echo "[$i] --> $mounted"
+        eval mounts$i="$mounted"
+        i=$((i+1))
 done
 
-if [ $i == "1" ] ; then
-  echo -e "$ERROR No $PART_TYPES partitions available. Exiting..."
-  exit 1
+if [ "$i" = "1" ] ; then
+        echo -e "$ERROR No $PART_TYPES partitions available. Exiting..."
+        exit 1
 fi
 
-echo -en "$INPUT Please enter partition number or 0 to exit\n$BOLD[0-`expr $i - 1`]$NORM: "
+echo -en "$INPUT Please enter partition number or 0 to exit\n$BOLD[0-$((i-1))]$NORM: "
 read partitionNumber
-if [ "$partitionNumber" == "0" ] ; then
-  echo -e $INFO Exiting...
-  exit 0
+if [ "$partitionNumber" = "0" ] ; then
+        echo -e "$INFO Exiting..."
+        exit 0
 fi
 
-if [ "$partitionNumber" -gt `expr $i - 1` ] ; then
-  echo -e "$ERROR Invalid partition number! Exiting..."
-  exit 1
+if [ "$partitionNumber" -gt $((i-1)) ] ; then
+        echo -e "$ERROR Invalid partition number! Exiting..."
+        exit 1
 fi
 
-eval entPartition=\$mounts$partitionNumber
+entPartition=""
+eval entPartition=\$mounts"$partitionNumber"
 echo -e "$INFO $entPartition selected.\n"
-entFolder=$entPartition/$ENT_FOLD
-entarmFolder=$entPartition/$ENT_FOLD
-entwareFolder=$entPartition/entware
-entwarearmFolder=$entPartition/entware.arm
-asuswareFolder=$entPartition/asusware
-asuswarearmFolder=$entPartition/asusware.arm
-optwareFolder=$entPartition/optware-ng
-optwarearmFolder=$entPartition/optware-ng.arm
+entwareFolder=$entPartition/$ENT_FOLD
+entFolder=$entPartition/$ENTNG_FOLD
+asuswareFolder=$entPartition/$OPT_FOLD
+optwareFolder=$entPartition/$OPTNG_FOLD
 
-if [ -d /opt/debian ]
-then
-  echo -e "$WARNING Found chrooted-debian installation, stopping..."
-  debian stop
+if [ -d /opt/debian ]; then
+        echo -e "$WARNING Found chrooted-debian installation, stopping debian..."
+        debian stop
 fi
 
-if [ -f /jffs/scripts/services-stop ]
-then
-  echo -e "$WARNING stopping running services..."
-  /jffs/scripts/services-stop
+if [ -f /jffs/scripts/services-stop ]; then
+        echo -e "$WARNING stopping running services..."
+        /jffs/scripts/services-stop
 fi
 
-if [ -d $entFolder ] ; then
-  echo -e "$WARNING Found previous entware-ng installation, saving..."
-  mv $entFolder $entFolder-bak_`date +\%F_\%H-\%M`
+if [ -d $entwareFolder ]; then
+        backup $entwareFolder $ENT_FOLD
 fi
 
-if [ -d $entarmFolder ] ; then
-  echo -e "$WARNING Found previous entware-ng.arm installation, saving..."
-  mv $entarmFolder $entarmFolder-bak_`date +\%F_\%H-\%M`
+if [ -d $entFolder ]; then
+        backup $entFolder $ENTNG_FOLD
 fi
 
-if [ -d $entwareFolder ] ; then
-  echo -e "$WARNING Found old entware installation, saving..."
-  mv $entwareFolder $entwareFolder-bak_`date +\%F_\%H-\%M`
+if [ -d $asuswareFolder ]; then
+        backup $asuswareFolder $OPT_FOLD
 fi
 
-if [ -d $entwarearmFolder ] ; then
-  echo -e "$WARNING Found previous entware-ng installation, saving..."
-  mv $entwarearmFolder $entwarearmFolder-bak_`date +\%F_\%H-\%M`
-fi
-
-if [ -d $asuswareFolder ] ; then
-  echo -e "$WARNING Found old optware installation, saving..."
-  mv $asuswareFolder $asuswareFolder-bak_`date +\%F_\%H-\%M`
-fi
-
-if [ -d $asuswarearmFolder ] ; then
-  echo -e "$WARNING Found old optware.arm installation, saving..."
-  mv $asuswarearmFolder $asuswarearmFolder-bak_`date +\%F_\%H-\%M`
-fi
-
-if [ -d $optwareFolder ] ; then
-  echo -e "$WARNING Found optware-ng installation, saving..."
-  mv $optwareFolder $optwareFolder-bak_`date +\%F_\%H-\%M`
-fi
-
-if [ -d $optwarearmFolder ] ; then
-  echo -e "$WARNING Found optware.ng.arm installation, saving..."
-  mv $optwarearmFolder $optwarearmFolder-bak_`date +\%F_\%H-\%M`
+if [ -d $optwareFolder ]; then
+        backup $optwareFolder $OPTNG_FOLD
 fi
 
 echo -e "$INFO Creating $entFolder folder..."
 mkdir $entFolder
 
-if [ -d /tmp/opt ] ; then
-  echo -e "$WARNING Deleting old /tmp/opt symlink..."
-  rm /tmp/opt
+if [ -d /tmp/opt ]; then
+        echo -e "$WARNING Deleting old /tmp/opt symlink..."
+        rm /tmp/opt
 fi
 
 echo -e "$INFO Creating /tmp/opt symlink..."
 ln -sf $entFolder /tmp/opt
 
 echo -e "$INFO Creating /jffs scripts backup..."
-tar -czf $entPartition/jffs_scripts_backup_`date +\%F_\%H-\%M`.tgz /jffs/scripts/* >/dev/nul
+tar -czf $entPartition/jffs_scripts_backup_"$(date +'%F_%H-%M')".tgz
+/jffs/scripts/* >/dev/nul
 
 echo -e "$INFO Modifying start scripts..."
 cat > /jffs/scripts/services-start << EOF
@@ -157,12 +154,12 @@ RC='/opt/etc/init.d/rc.unslung'
 
 i=30
 until [ -x "\$RC" ] ; do
-  i=\$((\$i-1))
-  if [ "\$i" -lt 1 ] ; then
-    logger "Could not start Entware-NG"
-    exit
-  fi
-  sleep 5
+        i=\$((\$i-1))
+        if [ "\$i" -lt 1 ] ; then
+                logger "Could not start Entware-NG"
+                exit
+        fi
+        sleep 5
 done
 \$RC start
 EOF
@@ -173,31 +170,66 @@ cat > /jffs/scripts/services-stop << EOF
 
 /opt/etc/init.d/rc.unslung stop
 EOF
+
 chmod +x /jffs/scripts/services-stop
+
+cat > /jffs/scripts/services-check << EOF
+#!/bin/sh
+
+/opt/etc/init.d/rc.unslung check
+EOF
+
+chmod +x /jffs/scripts/services-check
 
 cat > /jffs/scripts/post-mount << EOF
 #!/bin/sh
+TAG=\$(basename "\$0")_\$@
 
-if [ "\$1" = "__Partition__" ] ; then
-  ln -nsf \$1/$ENT_FOLD /tmp/opt
-fi
-
-sleep 2
-if [ -f /opt/swap ]
-then
-  echo -e "Mounting swap file..."
-  swapon /opt/swap
-else
-  echo -e "Swap file not found or /opt is not mounted..."
+if [ "\$1" = "$entPartition" ] ; then
+        ln -nsf \$1/$ENTNG_FOLD /tmp/opt
+        logger -t \$TAG "Created entware-ng symlink"
+        if [ -f /opt/swap ]; then
+                logger -t \$TAG "Mounting swap file..."
+                swapon /opt/swap
+        fi
 fi
 EOF
-eval sed -i 's,__Partition__,$entPartition,g' /jffs/scripts/post-mount
+
 chmod +x /jffs/scripts/post-mount
 
+cat > /jffs/scripts/pre-mount << EOF
+#!/bin/sh
+
+TAG=\$(basename "\$0")_\$@
+FSTYPE=\$(fdisk -l "${1:0:8}" | grep "$1" | cut -c55-65)
+
+case "\$FSTYPE" in
+        "Linux")
+                logger -t \$TAG "Checking linux filesystem"
+                LOG=\$(e2fsck -p \$1)
+                ;;
+        "Linux swap")
+                logger -t \$TAG "Mounting swap partition"
+                swapon \$1 && LOG="Swap partition mounted" || LOG="Device busy. Already mounted?"
+                ;;
+        "HPFS/NTFS")
+                logger -t \$TAG "Checking ntfs filesystem"
+                LOG=\$(ntfsck -a \$1)
+                ;;
+        *)
+                logger -t \$TAG "Unknow filesystem. Trying to check as ntfs filesystem"
+                LOG=\$(ntfsck -a \$1)
+                ;;
+esac
+logger -t \$TAG \$LOG
+EOF
+
+chmod +x /jffs/scripts/pre-mount
+
 if [ "$(nvram get jffs2_scripts)" != "1" ] ; then
-  echo -e "$INFO Enabling custom scripts and configs from /jffs..."
-  nvram set jffs2_scripts=1
-  nvram commit
+        echo -e "$INFO Enabling custom scripts and configs from /jffs..."
+        nvram set jffs2_scripts=1
+        nvram commit
 fi
 
 wget -qO - $INST_URL | sh
@@ -206,61 +238,48 @@ opkg install terminfo
 # Swap file
 while :
 do
-    clear
-    echo Router model `cat "/proc/sys/kernel/hostname"`
-    echo "---------"
-    echo "SWAP FILE"
-    echo "---------"
-    echo "Choose swap file size (Highly Recommended)"
-    echo "1. 512MB"
-    echo "2. 1024MB"
-    echo "3. 2048MB (recommended for MySQL Server or PlexMediaServer)"	
-    echo "4. Skip this step, I already have a swap file / partition"
-    echo "   or I don't want to create one right now"
-    read -p "Enter your choice [ 1 - 4 ] " choice
-    case $choice in
-        1) 
-            echo -e "$INFO Creating a 512MB swap file..."
-            echo -e "$INFO This could take a while, be patient..."
-            dd if=/dev/zero of=/opt/swap bs=1024 count=524288
-            mkswap /opt/swap
-            chmod 0600 /opt/swap
-			swapon /opt/swap
-            read -p "Press [Enter] key to continue..." readEnterKey
-			break
-            ;;
+        clear
+        echo -e "Router model $(cat "/proc/sys/kernel/hostname")"
+        echo "--------------------------------------------------"
+        echo "SWAP FILE CREATION"
+        echo "If you have a swap partition,"
+        echo "   the pre-mount script will mount it every reboot"
+        echo "   select 4 for skip SWAP FILE Creation"
+        echo "--------------------------------------------------"
+        echo "Choose swap file size (Highly Recommended)"
+        echo "1. 512MB"
+        echo "2. 1024MB"
+        echo "3. 2048MB (recommended for MySQL Server or PlexMediaServer)"
+        echo "4. Skip this step, I already have a swap file / partition"
+        echo "   or I don't want to create one right now"
+        echo -n "Enter your choice [ 1 - 4 ] : "
+        read choice
+        case $choice in
+        1)
+                swapfile 512
+                break
+                ;;
         2)
-            echo -e "$INFO Creating a 1024MB swap file..."
-            echo -e "$INFO This could take a while, be patient..."
-            dd if=/dev/zero of=/opt/swap bs=1024 count=1048576
-            mkswap /opt/swap
-            chmod 0600 /opt/swap
-			swapon /opt/swap
-            read -p "Press [Enter] key to continue..." readEnterKey
-			break
-            ;;
+                swapfile 1024
+                break
+                ;;
         3)
-            echo -e "$INFO Creating a 2048MB swap file..."
-            echo -e "$INFO This could take a while, be patient..."
-            dd if=/dev/zero of=/opt/swap bs=1024 count=2097152
-            mkswap /opt/swap
-            chmod 0600 /opt/swap
-			swapon /opt/swap
-            read -p "Press [Enter] key to continue..." readEnterKey
-			break
-            ;;			
+                swapfile 2048
+                break
+                ;;
         4)
-            break
-            ;;
+                break
+                ;;
         *)
-            echo "ERROR: INVALID OPTION!"			
-			echo "Press 1 to create a 512MB swap file"
-			echo "Press 2 to create a 1024MB swap file"
-			echo "Press 3 to create a 2048MB swap file (for Mysql or Plex)"			
-			echo "Press 4 to skip swap creation (not recommended)" 
-            read -p "Press [Enter] key to continue..." readEnterKey
-            ;;
-    esac	
+                echo "ERROR: INVALID OPTION!"
+                echo "Press 1 to create a 512MB swap file"
+                echo "Press 2 to create a 1024MB swap file"
+                echo "Press 3 to create a 2048MB swap file (for Mysql or Plex)"
+                echo "Press 4 to skip swap creation (not recommended)"
+                echo "Press [Enter] key to continue..."
+                read readEnterKey
+                ;;
+        esac
 done
 
 cat > /opt/bin/services << EOF
@@ -269,24 +288,28 @@ cat > /opt/bin/services << EOF
 export PATH=/opt/bin:/opt/sbin:/sbin:/bin:/usr/sbin:/usr/bin$PATH
 
 case "\$1" in
- start)
-   sh /jffs/scripts/services-start
-   ;;
- stop)
-   sh /jffs/scripts/services-stop
-   ;;
- restart)
-   sh /jffs/scripts/services-stop
-   echo -e Restarting Entware-NG Installed Services...
-   sleep 2
-   sh /jffs/scripts/services-start
-   ;;
- *)
-   echo "Usage: services {start|stop|restart}" >&2
-   exit 3
-   ;;
+        start)
+                sh /jffs/scripts/services-start
+                ;;
+        stop)
+                sh /jffs/scripts/services-stop
+                ;;
+        restart)
+                sh /jffs/scripts/services-stop
+                echo -e Restarting Entware-NG Installed Services...
+                sleep 2
+                sh /jffs/scripts/services-start
+                ;;
+        check)
+                sh /jffs/scripts/services-check
+                ;;
+        *)
+                echo "Usage: services {start|stop|restart|check}" >&2
+                exit 3
+                ;;
 esac
 EOF
+
 chmod +x /opt/bin/services
 
 cat << EOF
