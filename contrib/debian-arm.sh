3f549307a0574515246045d19b853c00b89984f8
NubeRoja <svalenciano@gmail.com>
2017-04-10 20:52:26 +0200

    Update debian-arm.sh

--- contrib/debian-arm.sh
+++ contrib/debian-arm.sh
@@ -14,8 +14,10 @@ echo -e "$INFO https://www.hqt.ro/how-to-install-debian-jessie-arm/"
 echo -e "$INFO with some tricks added by NubeRoja"
 echo -e "$WARNING This scripts depends on a previous installation of entware-ng or optware-ng"
 cd /opt || ( echo -e "$ERROR entware-ng or optware-ng not installed. Exiting" && exit 1 )
+echo -e "$INFO Downloading filesystem" 
 wget -c -O debian_jessie8.6-arm_clean.tgz http://goo.gl/Yp7CwA
-tar -xvzf ./debian_jessie8.6-arm_clean.tgz
+echo -e "$INFO Untar filesystem, please wait..."
+tar -xzf ./debian_jessie8.6-arm_clean.tgz
 
 echo -e "$INFO Creating debian init.d script"
 cat > /opt/etc/init.d/S99debian << EOF
@@ -55,10 +57,10 @@ stop() {
 		exit 1
 	fi
 	echo "Stopping chroot'ed Debian services..."
-        while IFS= read -r line; do
-                chroot "\$CHROOT_DIR" "/etc/init.d/\$line" stop
+	while IFS= read -r line; do
+		chroot "\$CHROOT_DIR" "/etc/init.d/\$line" stop
 		sleep 2
-        done < \$CHROOT_SERVICES_LIST
+	done < \$CHROOT_SERVICES_LIST
 	mount | grep \$CHROOT_DIR | awk '{print \$3}' | xargs umount -l
 }
 	
@@ -72,12 +74,12 @@ restart() {
 			chroot "\$CHROOT_DIR" "/etc/init.d/\$line" stop
 			sleep 2
 		done < \$CHROOT_SERVICES_LIST
-	        mount | grep \$CHROOT_DIR | awk '{print \$3}' | xargs umount -l
+		mount | grep \$CHROOT_DIR | awk '{print \$3}' | xargs umount -l
 		echo "Restarting chroot'ed Debian services..."
-	        for dir in dev proc sys; do
-		        mount -o bind /\$dir \$CHROOT_DIR/\$dir
-	        done
-	        [ -z "\$EXT_DIR" ] || mount -o bind \$EXT_DIR \$CHROOT_DIR/mnt
+	  for dir in dev proc sys; do
+			mount -o bind /\$dir \$CHROOT_DIR/\$dir
+	  done
+	  [ -z "\$EXT_DIR" ] || mount -o bind \$EXT_DIR \$CHROOT_DIR/mnt
 		while IFS= read -r line; do
 			chroot "\$CHROOT_DIR" "/etc/init.d/\$line" start
 			sleep 2
@@ -141,7 +143,27 @@ sed -i -e 's/# es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /opt/debian/etc/locale.gen
 sed -i -e 's/# eval \"`dircolors`\"/eval \"`dircolors`\"/' /opt/debian/root/.bashrc
 sed -i -e 's/# alias l/alias l/' /opt/debian/root/.bashrc
 
-echo -e "$INFO Setup complete. debian $(debian)"
-echo -e "$INFO Please execute debian enter &"
-echo -e "$INFO apt update && apt upgrade -y
-exit
+while : ; do
+	clear
+  echo -e "$INFO Setup complete. debian $(debian)"
+  echo "--------------------------------------------------"
+  echo -n "Want to updrade debian packages? [ y / n ]: "
+  read choice
+  case $choice in
+  	y|Y)
+			echo -e "$INFO Starting debian"
+			debian start
+			echo -e "
+			chroot \$CHROOT_DIR apt update && apt upgrade -y
+      ;;
+    n|N)
+			exit 0
+    *)
+			echo "Type y to update debian and finish installation"
+      echo "Type n to finish installation without update"
+			echo "Press Enter key to continue"
+      read readEnterKey
+      ;;
+	esac
+done
+exit 0
