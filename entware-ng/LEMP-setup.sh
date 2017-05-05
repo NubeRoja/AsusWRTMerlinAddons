2a078fbe0e62a90dc27eb6d3a29523e0cf672d8e
NubeRoja <svalenciano@gmail.com>
2017-05-05 15:24:38 +0200

    Update LEMP-setup.sh

--- entware-ng/LEMP-setup.sh
+++ entware-ng/LEMP-setup.sh
@@ -5,6 +5,11 @@ INFO="${BOLD}Info: $NORM"
 WARNING="${BOLD}* Warning: $NORM"
 INPUT="${BOLD}=> $NORM"
 
+getgithubraw () {
+	[ -f "$2" ] && mv "$2" "$2-opkg"
+	wget -O "$2" "https://raw.githubusercontent.com/NubeRoja/AsusWRTMerlinAddons/master$2"
+}
+
 clear
 echo -e "${INFO}This script was created by NubeRoja."
 echo -e "${INFO}But is based on tutorials around the internet"
@@ -29,22 +34,19 @@ phpfpmINST=$(opkg list-installed | awk '{print $1}' | grep -q "php5-fpm" && echo
 mysqlINST=$(opkg list-installed | awk '{print $1}' | grep -q "mysql-server" && echo true || echo false)
 
 if $nginxINST; then
-	echo -e "${WARNING}Nginx already installed, saving '/opt/etc/nginx/nginx.conf' & '/opt/etc/nginx/sites-available'"
-	[ -f /opt/etc/nginx/nginx.conf ] && cp "/opt/etc/nginx/nginx.conf" "/opt/etc/nginx/nginx.conf.PRELEMP"
-	[ -d /opt/etc/nginx/sites-available ] && mv "/opt/etc/nginx/sites-available" "/opt/etc/nginx/sites-available.PRELEMP"
-	[ -d /opt/etc/nginx/sites-enabled ] && rm -r "/opt/etc/nginx/sites-enabled/*"
-	opkg remove nginx --autoremove
+	echo -e "${WARNING}Nginx already installed, saving '/opt/etc/nginx/nginx.conf-PRELEMP' & '/opt/etc/nginx/sites-available-PRELEMP'"
+	[ -f /opt/etc/nginx/nginx.conf ] && cp /opt/etc/nginx/nginx.conf /opt/etc/nginx/nginx.conf-PRELEMP
+	[ -d /opt/etc/nginx/sites-available ] && mv /opt/etc/nginx/sites-available /opt/etc/nginx/sites-available-PRELEMP
+	[ -d /opt/etc/nginx/sites-enabled ] && rm -r /opt/etc/nginx/sites-enabled
 fi
-mkdir -p "/opt/etc/nginx/sites-available"
-mkdir -p "/opt/etc/nginx/sites-enabled"
 
 if $phpfpmINST; then
-	echo -e "${WARNING}php5-fpm already installed, saving '/opt/etc/php5-fpm.d.PRELEMP/'"
-	[ -f /opt/etc/php5-fpm.d/www.conf ] && cp /opt/etc/php5-fpm.d/ /opt/etc/php5-fpm.d.PRELEMP/
+	echo -e "${WARNING}php5-fpm already installed, saving '/opt/etc/php5-fpm.d-PRELEMP/'"
+	[ -f /opt/etc/php5-fpm.d/www.conf ] && mv /opt/etc/php5-fpm.d/ /opt/etc/php5-fpm.d-PRELEMP/ && mkdir -p /opt/etc/php5-fpm.d/
 fi
 
 if $phpINSTt; then
-	echo -e "${WARNING}php5 already installed, saving '/opt/etc/php.ini.PRELEMP'"
+	echo -e "${WARNING}php5 already installed, saving '/opt/etc/php.ini-PRELEMP'"
 	[ -f /opt/etc/php.ini ] && cp /opt/etc/php.ini /opt/etc/php.ini.PRELEMP
 fi
 
@@ -53,172 +55,25 @@ if $mysqlINST; then
 	[ -f /opt/etc/my.cnf ] && cp /opt/etc/my.cnf /opt/etc/my.cnf.PRELEMP
 fi
 
-opkg install nginx --force-overwrite && echo -e "${INFO}Nginx installed Ok, configuring..."
-cat > /opt/etc/nginx/nginx.conf << EOF
-user  nobody nobody;
-worker_processes  1;
-
-#error_log  /opt/var/log/nginx/error.log;
-#error_log  /opt/var/log/nginx/error.log  notice;
-#error_log  /opt/var/log/nginx/error.log  info;
-
-#pid        /opt/var/run/nginx.pid;
-
-events {
-        worker_connections  1024;
-}
-
-http {
-        include       mime.types;
-        default_type  text/html;
-
-        #log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-        #                  '\$status \$body_bytes_sent "\$http_referer" '
-        #                  '"\$http_user_agent" "\$http_x_forwarded_for"';
-
-        #access_log  /opt/var/log/nginx/access.log main;
-
-        server_names_hash_bucket_size 64;
-
-        sendfile        on;
-        #tcp_nopush     on;
-
-        #keepalive_timeout  0;
-        keepalive_timeout  65;
-
-        gzip  on;
-        gzip_min_length  1k;
-        gzip_buffers     4 16k;
-        gzip_http_version 1.0;
-        gzip_comp_level 2;
-        gzip_types       text/plain application/x-javascript text/css application/xml;
-        gzip_vary on;
-
-        include /opt/etc/nginx/sites-enabled/*;
-}
-EOF
-
-cat > /opt/etc/nginx/sites-available/default << EOF
-server {
-        listen 80;
-        server_name calambre.local www.calambre.local;
-
-        root $wwwdir;
-        index index.php index.html index.htm;
-        try_files \$uri \$uri/ /404.html;
-
-        proxy_buffers 16 16k;
-        proxy_buffer_size 16k;
-        client_body_timeout 10;
-        client_header_timeout 10;
-        send_timeout 60;                # 60 sec should be enough, if experiencing a lot of timeouts, increase this.
-        output_buffers 1 32k;
-        postpone_output 1460;
-
-        location = /favicon.ico {
-                log_not_found off;
-                access_log off;
-        }
-
-        location = /robots.txt {
-                allow all;
-                log_not_found off;
-                access_log off;
-        }
-
-        location ~ /\\. {
-                deny all;
-                access_log off;
-                log_not_found off;
-        }
-
-        location ~*  \\.(jpg|jpeg|png|gif|css|js|ico)\$ {
-                expires max;
-                log_not_found off;
-        }
-
-        location ~ \\.php\$ {
-                fastcgi_connect_timeout 300;
-                fastcgi_send_timeout 300;
-                fastcgi_read_timeout 300;
-                fastcgi_buffer_size 32k;
-                fastcgi_buffers 4 32k;
-                fastcgi_busy_buffers_size 32k;
-                fastcgi_temp_file_write_size 32k;
-                fastcgi_pass unix:/var/run/php5-fpm.sock;
-                fastcgi_index index.php;
-                fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-                include       fastcgi_params;
-        }
-}
-EOF
+opkg install nginx && echo -e "${INFO}Nginx installed Ok, configuring..."
+getgithubraw "/entware-ng/LEMP-config/nginx.conf" "/opt/etc/nginx.conf"
+mkdir -p "/opt/etc/nginx/sites-available"
+mkdir -p "/opt/etc/nginx/sites-enabled"
+getgithubraw "/entware-ng/LEMP-config/default" "/opt/etc/nginx/sites-available/default"
 cd /opt/etc/nginx/sites-enabled
 ln -sf ../sites-available/default default
-
-cat > /opt/etc/init.d/S80nginx << EOF
-#!/bin/sh
-ansi_red="\033[1;31m";
-ansi_white="\033[1;37m";
-ansi_green="\033[1;32m";
-ansi_std="\033[m";
-ENABLED=yes
-PROCS=nginx
-PREARGS=""
-DESC=Nginx
-
-PATH=/opt/sbin:/opt/bin:/opt/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-case \$1 in
-	start)
-		ARGS=""
-		. /opt/etc/init.d/rc.func
-		;;
-	stop | kill)
-		ARGS="-s quit"
-		. /opt/etc/init.d/rc.func
-		;;
-	restart)
-		ARGS="-s quit"
-		. /opt/etc/init.d/rc.func stop
-		ARGS=""
-		. /opt/etc/init.d/rc.func start
-		;;
-	check)
-		ARGS=""
-		. /opt/etc/init.d/rc.func
-		;;
-	reload)
-		echo -e -n "\$ansi_white Reloading Nginx conf...       "
-		nginx -s reload && echo -e "\$ansi_green done." || echo -e "\$ansi_red failed.\$ansi_std"
-		;;
-		reopen)
-		echo -e -n "\$ansi_white Reopening Nginx log files...  "
-		nginx -s reopen && echo -e "\$ansi_green done." || echo -e "\$ansi_red failed.\$ansi_std"
-		;;
-	test)
-		nginx -t
-		;;
-	*)
-		echo -e "\$ansi_white Usage: \$0 (start|stop|restart|check|kill|reload|reopen|test)\$ansi_std"
-		;;
-esac
-EOF
+getgithubraw "/entware-ng/LEMP-config/S80nginx" "/opt/etc/init.d/S80nginx"
 chmod 755 /opt/etc/init.d/S80nginx
+[ -f /opt/etc/init.d/S80nginx-opkg ] && chmod 555 /opt/etc/init.d/S80nginx-opkg
 
-opkg install php5-fpm --force-overwrite && echo -e "${INFO}php5-fpm installed Ok, configuring..."
-
-#sed -i 's_doc_root =  "$wwwdir"_doc_root = "$wwwdir"_g' "/opt/etc/php.ini"
-sed -i 's,session.save_path = "/opt/tmp",session.save_path = "/opt/tmp/php",g' "/opt/etc/php.ini"
+opkg install php5-fpm && echo -e "${INFO}php5-fpm installed Ok, configuring..."
+getgithubraw "/entware-ng/LEMP-config/php.ini" "/opt/etc/php.ini"
 mkdir -p /opt/tmp/php
 chmod 755 /opt/tmp/php
-sed -i 's_;listen = /var/run/php5-fpm.sock_listen = /var/run/php5-fpm.sock_g' "/opt/etc/php5-fpm.d/www.conf"
-sed -i 's_listen = 127.0.0.1:9000_;listen = 127.0.0.1:9000_g' "/opt/etc/php5-fpm.d/www.conf"
-sed -i 's_;listen.owner = www-data_listen.owner = nobody_g' "/opt/etc/php5-fpm.d/www.conf"
-sed -i 's_;listen.group = www-data_listen.group = nobody_g' "/opt/etc/php5-fpm.d/www.conf"
+getgithubraw "/entware-ng/LEMP-config/www.conf" "/opt/etc/php5-fpm.d/www.conf"
 
-opkg install mysql-server --force-overwrite && echo -e "${INFO}mysql-server installed Ok, configuring..."
-
-sed -E -i 's_socket[[:space:]]+= /opt/var/run/mysqld.sock_socket          = /var/run/mysqld.sock_g' "/opt/etc/my.cnf"
+opkg install mysql-server && echo -e "${INFO}mysql-server installed Ok, configuring..."
+getgithubraw "/entware-ng/LEMP-config/my.cnf" "/opt/etc/my.cnf"
 opkg install php5-mod-mysqli php5-mod-session php5-mod-mbstring php5-mod-json
 mysql_install_db --force
 
